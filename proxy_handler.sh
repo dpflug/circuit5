fc88d07e165eccba745f0492406bb3894baba376
David Pflug <david@tpflug.com>
2016-08-04 08:29:40 -0400

    proxy_handler: Allow for multiple connections
    Also, undo octet match change and remove some debugging

--- proxy_handler.sh
+++ proxy_handler.sh
@@ -11,25 +11,25 @@ log="/home/dpflug/CONNECT_C5"
 
 if [ "$SSID" = "C5" ] || [ "$SSID" = "C5ENT" ] ; then
     su - dpflug /home/dpflug/bin/connect_c5.sh || true
-    SECOND_OCTET="$(ip a | grep -oP '(?<=\b10.)1\d{2,3}(?=.\d{1,3}.\d{1,3}/)')"
+    SECOND_OCTET="$(ip a | grep -oP '(?<=\b10.)1\d{2,3}(?=.\d{1,3}.\d{1,3}/)' | head -n1)"
     echo "Starting octet match." >> $log
     echo "Second octet: $SECOND_OCTET" >> $log
     #if [ "$Profile" = "wlp3s0-C5" ] ||
     #       [ "$Profile" = "wlp3s0-C5ENT" ] ||
     #       [ "$Profile" = "eno1-dhcp" ] && [ "${IP%.*.*.*}" = "10" ] ; then
-    if [ "$SECOND_OCTET" -eq 128 ] ; then
+    if [ "$SECOND_OCTET" = 128 ] ; then
 	# In Citrus
 	COUNTY="cit"
-    elif [ "$SECOND_OCTET" -eq 129 ] ; then
+    elif [ "$SECOND_OCTET" = 129 ] ; then
 	# In Hernando
 	COUNTY="her"
-    elif [ "$SECOND_OCTET" -eq 130 ] ; then
+    elif [ "$SECOND_OCTET" = 130 ] ; then
 	# In Lake
 	COUNTY="lak"
-    elif [ "$SECOND_OCTET" -eq 131 ] ; then
+    elif [ "$SECOND_OCTET" = 131 ] ; then
 	# In Marion
 	COUNTY="mar"
-    elif [ "$SECOND_OCTET" -eq 132 ] ; then
+    elif [ "$SECOND_OCTET" = 132 ] ; then
 	# In Sumter	
 	COUNTY="sum"
     fi
@@ -37,8 +37,6 @@ if [ "$SSID" = "C5" ] || [ "$SSID" = "C5ENT" ] ; then
 fi
 
 if [ "$COUNTY" ] ; then
-    { echo -n "Detected county: ";
-      echo "$COUNTY"; } >> $log
     sed -i.bk 's/#include/include/' /etc/unbound/unbound.conf
     systemctl reload unbound
     sed -i.bk "/^Upstream/s/[a-z]*proxy/${COUNTY}proxy/" /etc/tinyproxy/tinyproxy.conf
