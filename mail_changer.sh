5660f0a1348d22027d39775e2e2e8f7114ca1574
David Pflug <david@tpflug.com>
2016-11-04 08:58:04 -0400

    mail_changer: Add after-hours support, change offset
      Offset is now number of hours

--- mail_changer.sh
+++ mail_changer.sh
@@ -18,6 +18,7 @@ setmail() {
 }
 
 SHIFT_CHANGE=1230
+SHIFTS_END=1730
 DESK_JOCKEYS=(
     # First option is who to send to by default
     # We default to the entire Court Tech department
@@ -52,7 +53,7 @@ while true; do
 	    echo "Usage: mail_changer.sh [-t] [-oOFFSET]"
 	    echo -e "  Arguments:"
 	    echo -e "    -t, --test\t\t\tOutput a 10 day period (optionally, around offset) for testing."
-	    echo -e "    -oOFFSET, --offset=OFFSET\tNumber of 12 hour periods (positive or negative) to offset."
+	    echo -e "    -oOFFSET, --offset=OFFSET\tNumber of hours (positive or negative) to offset."
 	    exit
 	    ;;
 	-d|--debug)
@@ -100,20 +101,27 @@ set_vars() {
 	# Weekend; use default
 	SHIFT=0
     else
-	DAY=$(( $(date +%w --date="$1") + (WEEK % SHIFT_COUNT) * 5 )) # Day of cycle
+	# Which day of the cycle is it?
+	DAY=$(( $(date +%w --date="$1") + (WEEK % SHIFT_COUNT) * 5 ))
+	# Is it after SHIFT_CHANGE?
 	SHIFT_OFFSET=$(( ($(date +%k%M --date="$1") < SHIFT_CHANGE)?0:1 ))
-	SHIFT=$(( (DAY % SHIFT_COUNT) * 2 + 1 + SHIFT_OFFSET ))
+	# Is it after SHIFTS_END?
+	AFTER_HOURS=$(( ($(date +%k%M --date="$1") < SHIFTS_END)?0:1 ))
+	# Pick our shift from the list
+	SHIFT=$(( AFTER_HOURS?0:((DAY % SHIFT_COUNT) * 2 + 1 + SHIFT_OFFSET) ))
     fi
 }
 
 if [ "$TEST" == 1 ] ; then
-    for i in {-10..10} ; do
-	when="+ $(((OFFSET + i) * 12)) hours"
-	set_vars "$when"
+    for d in {-5..5} ; do
+	for t in "0000" "$SHIFT_CHANGE" "$SHIFTS_END" ; do
+	    when="+ $d days $t + $OFFSET hours"
+	    set_vars "$when"
 	    
-	echo "$(date --date="$when")" "-- ${DESK_JOCKEYS[$SHIFT]}"
+	    echo "$(date --date="$when")" "-- ${DESK_JOCKEYS[$SHIFT]}"
+	done
     done
 else
-    set_vars "+ $((OFFSET * 12)) hours"
+    set_vars "+ $OFFSET hours"
     setmail "${DESK_JOCKEYS[$SHIFT]}"
 fi
